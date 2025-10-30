da240e383f88fcf35344489f5006879f538ed938
Luis J. Rodriguez Espinal <luisjrodrigueze@gmail.com>
2025-10-30 13:39:44 -0400

    fixing typo script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.addEventListener("scroll", () => {
     }
   });
 
-  navLinks, forEach((link)=> {
+  navLinks.forEach((link)=> {
     link.classList.remove("active");
     if (link.getAttribute("href") === "#" + current) {
       link.classList.add("active");
